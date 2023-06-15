3ad61f9b29df438f2056506bced708f2614f3afa
shrustic1 <116289967+shrustic1@users.noreply.github.com>
2023-06-16 01:22:30 +0200

    all in one function

--- inverse_matrix_LU.cpp
+++ inverse_matrix_LU.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int dimension;  //ostaje globalna varijabla, jer se nigdje ne prenosi u funkciji, moze se izmijeniti da se i prenosi kao parametar u funkciji logy
-
-const int N = 100; //moze se povecati, na pocetku se alocira vektor ove duzine, a u mainu se trazi tacna dimenzija matrice
-
-double *mat[N];
-double *lower[N], *upper[N], *inverse[N], *Z[N], *I[N];;
-
-void lu()
+std::vector<std::vector<double>> inverse_matrix(std::vector<std::vector<double>> matrix, int dimension)
 {
+    std::vector<std::vector<double>> inverse_matrix(matrix.size(), std::vector<double> (matrix.size()));
+    std::vector<std::vector<double>> lower(matrix.size(), std::vector<double> (matrix.size()));
+    std::vector<std::vector<double>> upper(matrix.size(), std::vector<double> (matrix.size()));
+    std::vector<std::vector<double>> Z(matrix.size(), std::vector<double> (matrix.size()));
+    std::vector<std::vector<double>> I(matrix.size(), std::vector<double> (matrix.size()));
+
+    for (int i=0; i<dimension; i++){
+      for (int j=0; j<dimension; j++){
+        if (i==j) I[i][i]=1;
+        else I[i][j]=0;
+        Z[i][j]=0;
+      }
+    }
+  
     int i = 0, j = 0, k = 0;
     for (i = 0; i < dimension; i++)
     {
@@ -20,7 +28,7 @@ void lu()
                 lower[j][i] = 0;
             else
             {
-                lower[j][i] = mat[j][i];
+                lower[j][i] = matrix[j][i];
                 for (k = 0; k < i; k++)
                 {
                     lower[j][i] = lower[j][i] - lower[j][k] * upper[k][i];
@@ -35,143 +43,63 @@ void lu()
                 upper[i][j] = 1;
             else
             {
-                upper[i][j] = mat[i][j] / lower[i][i];
+                upper[i][j] = matrix[i][j] / lower[i][i];
                 for (k = 0; k < i; k++)
                 {
                     upper[i][j] = upper[i][j] - ((lower[i][k] * upper[k][j]) / lower[i][i]);
                 }
             }
         }
-    }
-}
-
-void output(int *matrix[dimension])
-{
-    int i = 0, j = 0;
-    for (i = 0; i < dimension; i++)
-    {
-        for (j = 0; j < dimension; j++)
-        {
-            cout << matrix[i][j] << '\t';
-        }
-        cout << endl;
-    }
-}
-
-
-void output(float *matrix[dimension])
-{
-    int i = 0, j = 0;
-    for (i = 0; i < dimension; i++)
-    {
-        for (j = 0; j < dimension; j++)
-        {
-            cout << matrix[i][j] << '\t';
-        }
-        cout << endl;
-    }
-}
-
-void output(double *matrix[dimension])
-{
-    int i = 0, j = 0;
-    for (i = 0; i < dimension; i++)
-    {
-        for (j = 0; j < dimension; j++)
-        {
-            cout << matrix[i][j] << '\t';
-        }
-        cout << endl;
-    }
-}
-
-double compute_z(int col, int row) {
-    double sum = 0;
-    for(int i = 0; i < dimension; i++) {
-        if(i != row) {
-            sum += lower[row][i] * Z[i][col];
-        }
-    }
-
-    double result = I[row][col] - sum;
-    result = result / lower[row][row];
-
-    return result;
-}
-
-double compute_inverse(int col, int row) {
-    double sum = 0;
-    for(int i = 0; i < dimension; i++) {
-        if(i != row) {
-            sum += upper[row][i] * inverse[i][col];
-        }
-    }
-
-    double result = Z[row][col] - sum;
-    result = result / upper[row][row];
-
-
-    return result;
-}
-
-
-void inverse_matrix() {
-
+    } 
+  
     // compute z
     for(int col = 0; col < dimension; col++) {
         for(int row = 0; row < dimension; row++) {
-            Z[row][col] = compute_z(col, row);
-        }
-    }
+            double sum = 0;
+            for(int i = 0; i < dimension; i++) {
+                if(i != row) {
+                    sum += lower[row][i] * Z[i][col];
+                }
+            }
+                Z[row][col] = (I[row][col] - sum)/lower[row][row];
+                }
+            }
 ;
     // compute inverse
     for(int col = 0; col < dimension; col++) {
         for(int row = dimension - 1; row >= 0; row--) {
-            inverse[row][col] = compute_inverse(col, row);
+          double sum = 0;
+          for(int i = 0; i < dimension; i++) {
+              if(i != row) {
+                  sum += upper[row][i] * inverse_matrix[i][col];
+              }
+          }
+            inverse_matrix[row][col] = (Z[row][col] - sum)/ upper[row][row];
         }
     }
+  
+    return inverse_matrix;
 }
 
-// Driver code
 int main() {
 
-    cout<< "Input dimension of a matrix: ";
+  double dimension; 
+  
+    //cout<< "Input dimension of a matrix: ";
     cin>>dimension;
   
-    for (int i = 0; i < dimension; i++) {
-        mat[i] = new double[dimension];
-        lower[i] = new double[dimension];
-        upper[i] = new double[dimension];
-        inverse[i] = new double[dimension];
-        Z[i] = new double[dimension];
-        I[i] = new double[dimension];
-    }
-  
-    cout << "Input a matrix: " << endl;
+    //cout << "Input a matrix: " << endl;
 
-    for (int i = 0; i < dimension; i++) {
-        for (int j = 0; j < dimension; j++) {
-            cin >> mat[i][j];
-            Z[i][j] = 0;
-            if(i == j)
-                I[i][j] = 1;
-        }
+    std::vector<std::vector<double>> mat{{1,2,3},{4,5,6}, {7,8,8}};
+  std::vector<std::vector<double>> inv_mat = inverse_matrix(mat, dimension);
+    
+    cout << "Inverse matrix" << endl;
+    for (int i=0; i<dimension; i++){
+      for (int j=0; j<dimension; j++){
+        std::cout<<inv_mat[i][j]<<" ";
+      }
+      std::cout<<"\n";
     }
-
-    lu();
-    inverse_matrix();
-    
-    cout << "Matrix" << endl;
-    output(mat);
-    cout << endl << "Lower " << endl;
-    output(lower);
-    cout << endl <<  "Upper " << endl;
-    output(upper);
-    cout << endl <<  "Z " << endl;
-    output(Z);
-    
-    cout << endl << "inverse " << endl;
-    output(inverse);
 
     return 0;
 }
